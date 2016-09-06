9520ce6a72aa219ae99a940898cabdb2b7dddf78
mclandlord <marc@marccianciola.com>
2016-09-06 15:10:42 -0400

    Removed file that shouldn't be tracked

---
+++ NavCtrl/DerivedData/NavCtrl/Build/Intermediates/NavCtrl.build/Debug-iphonesimulator/NavCtrl.build/Objects-normal/i386/ProductViewController.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/marc/Documents/Xcode/TurnToTech\ Projects/Part\ 7\ Nav\ Control/NavCtrl/ProductViewController.m \
+  /Users/marc/Documents/Xcode/TurnToTech\ Projects/Part\ 7\ Nav\ Control/NavCtrl/ProductViewController.h \
+  /Users/marc/Documents/Xcode/TurnToTech\ Projects/Part\ 7\ Nav\ Control/NavCtrl/NavCtrl/WebViewController.h
---
+++ NavCtrl/DerivedData/NavCtrl/Build/Intermediates/NavCtrl.build/Debug-iphonesimulator/NavCtrl.build/Objects-normal/i386/WebViewController.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/marc/Documents/Xcode/TurnToTech\ Projects/Part\ 7\ Nav\ Control/NavCtrl/NavCtrl/WebViewController.m \
+  /Users/marc/Documents/Xcode/TurnToTech\ Projects/Part\ 7\ Nav\ Control/NavCtrl/NavCtrl/WebViewController.h
